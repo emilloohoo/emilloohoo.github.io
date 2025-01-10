17bc1c16a4ebe912b1a6caddd01811c3e9651b6b
Emily Brent <emilybrent@emilys-mbp-2.lan>
2025-01-09 21:42:18 -0600

    theme update

--- _site/assets/js/site.js
+++ _site/assets/js/site.js
@@ -1,47 +1,18 @@
 /**
  * Menu
  */
-$("a.menu-icon").on("click", function(event) {
-  var w = $(".menu");
+ $("a.menu-icon").on("click", function(event) {
+   var w = $(".menu");
 
-  w.css({
-    display: (w.css("display") === "none")
+   w.css({
+     display: w.css("display") === "none"
       ? "block"
       : "none"
-  });
-});
+   });
+ });
 
 /**
- * Footer year
- */
-$(document).ready(function() {
-  $("#year").text(new Date().getFullYear());
-});
-
-/**
- * Footer email
- */
-$(document).ready(function() {
-  $(".email-link-cloaked").on("click", function(event) {
-    const _link = event.target;
-
-    const _user = reverseString(_link.getAttribute("data-resu"));
-    const _domain = reverseString(_link.getAttribute("data-eman-niamod"));
-    const _tld = reverseString(_link.getAttribute("data-dlt-niamod"));
-
-    window.location.href = `mailto:${_user}@${_domain}.${_tld}`;
-  });
-});
-
-/**
- * Reverses a string
- */
-function reverseString(str) {
-  return str.split("").reverse().join("");
-}
-
-/**
- * Moves WeChat widget
+ * Wechat widget
  */
 function moveWidget(event) {
   var w = $("#wechat-widget");
@@ -54,7 +25,6 @@ function moveWidget(event) {
 
 $("a#wechat-link").on("mouseenter", function(event) {
   $("#wechat-widget").css({ display: "block" });
-
   moveWidget(event);
 });
 
